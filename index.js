d6988937b8b482eea76284b96e64ec8e63777a90
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-11-18 15:04:26 -0500

    added prompt questions

--- index.js
+++ index.js
@@ -1,23 +1,68 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-// const fs = require('fs');
+const fs = require('fs');
 // const {writeFile} = require('./utils/generateMarkdown.js');
 
 
 // TODO: Create an array of questions for user input
-// const questions = () => {
-    // return 
-    inquirer.prompt([
+const questions = () => {
+    return inquirer.prompt([
     {
         type: 'input',
         name: 'name',
-        message: 'What is the name of the developer?'
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description of the project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose which licences your project has.',
+        choices: ['MIT', 'Other1', 'Other2','Other3','Other4']
+        // what other licenses are there?????
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What steps are needed to install this project?'
+        // What steps ARE needed, anyway???
+    },
+    {
+        type: 'input',
+        name: 'useage',
+        message: 'How does one use this app?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are the test instructions?'
+        // not sure what this even means??
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'What are the contribution guidelines?'
+        // not sure what this even means??
     }
-    ])
-    .then(answer => console.log(answer));
-// }
+    ]);
+    
+};
 
 
+questions()
 
 
 // // TODO: Create a function to write README file
